c0d1f72375c5a1ab6f734a716a435aa0d22b0678
ishan <scifyfuntime@gmail.com>
2021-01-13 09:25:27 +0530

    new commit

--- screens/SL.js
+++ screens/SL.js
@@ -1,15 +1,21 @@
 import * as React  from 'react';
-import {View, Alert, TouchableOpacity, TextInput, StyleSheet, Text} from 'react-native';
+import {View, Alert, TouchableOpacity, TextInput, StyleSheet, Text, Modal, ScrollView, KeyboardAvoidingView} from 'react-native';
 import db from '../Config.js';
 import firebase from 'firebase';
 
 export default class SL extends React.Component {
 
     constructor(){
-        super()
+        super();
+
         this.state={
-          username : '',
-          password: ''
+          emailId:'',
+          password:'',
+          username:'',
+          address:'',
+          contact:'',
+          confirmPassword:'',
+          isModalVisible: false
         }
       }
 
@@ -25,21 +31,147 @@ export default class SL extends React.Component {
         })
     }
 
-    userSignUp = (username, password)=>{
-        firebase.auth().createUserWithEmailAndPassword(username, password)
-        .then(()=>{
-            return Alert.alert('Account created');
-        })
-        .catch((error)=>{
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            return Alert.alert(errorMessage)
-        })
+    userSignUp = (username, password, confirmPassword)=>{
+
+        if(confirmPassword !== password){
+            Alert.alert("The password doesn't match")
+        }else{
+
+            firebase.auth().createUserWithEmailAndPassword(username, password)
+            .then((response)=>{
+                db.collection(Users).add({
+                    user : this.state.username,
+                    mobile_number : this.state.contact,
+                    address : this.state.address,
+                    email : this.state.emailId
+                })
+                return Alert.alert('Account created',
+                '',
+                [{text:'Ok', onPress: ()=> this.setState({isModalVisible:false})}])   
+            })
+            .catch((error)=>{
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                return Alert.alert(errorMessage)
+            })
+        }
+    }
+
+    showModal = ()=>{
+        <Modal
+        animationType = 'none'
+        transparent = {true}
+        visible = {this.state.isModalVisible}
+       >
+
+            <View style={styles.modalContainer}>
+                <ScrollView style={{width:'100%'}}>
+                    <KeyboardAvoidingView style={styles.keyboardAvoidingView}>
+                        <Text style={styles.modalTitle}>Sign Up</Text>
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder='Username'
+                            onChangeText={(text)=>{
+                                this.setState({
+                                    username: text
+                                })
+                            }}
+                        />
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder='Email'
+                            keyboardType = 'email-address'
+
+                            onChangeText={(text)=>{
+                                this.setState({
+                                    emailId: text
+                                })
+                            }}
+                        />
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder='Password'
+                            secureTextEntry = {true}
+                            keyboardType = 'number-pad'
+
+                            onChangeText={(text)=>{
+                                this.setState({
+                                    password: text
+                                })
+                            }}
+                        />
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder=' Confirm Password'
+                            secureTextEntry = {true}
+                            keyboardType = 'number-pad'
+
+                            onChangeText={(text)=>{
+                                this.setState({
+                                   confirmPassword: text
+                                })
+                            }}
+                        />
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder='Adress'
+                            onChangeText={(text)=>{
+                                this.setState({
+                                    address: text
+                                })
+                            }}
+                        />
+
+                        <TextInput
+                            style={styles.form}
+                            placeholder='Contact'
+                            keyboardType = 'number-pad'
+
+                            onChangeText={(text)=>{
+                                this.setState({
+                                    contact: text
+                                })
+                            }}
+                        />
+
+                        <View style={styles.modalBackButton}>
+                            <TouchableOpacity
+                                style={styles.registerButton}
+                                onPress={()=>
+                                this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+                                }
+                            >
+                            <Text style={styles.registerButtonText}>Register</Text>
+                            </TouchableOpacity>
+                        </View>
+                        
+                        <View style={styles.modalBackButton}>
+                            <TouchableOpacity
+                                style={styles.cancelButton}
+                                onPress={()=>this.setState({"isModalVisible":false})}
+                            >
+                            <Text style={{color:'#ff5722'}}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </KeyboardAvoidingView>
+                </ScrollView>
+            </View>
+        </Modal>
     }
 
     render(){
         return(
             <View style={styles.container}>
+
+                <View>
+                    {this.showModal()}
+                </View>
 
                 <View style={styles.buttonContainer}>
 
@@ -75,12 +207,12 @@ export default class SL extends React.Component {
                     ><Text style={styles.buttonText}>Login</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={()=>{this.userSignUp(this.state.username, this.state.password)}}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={()=>this.setState({ isModalVisible:true})}
 
                     ><Text style={styles.buttonText}>SignUp</Text>
-                    </TouchableOpacity>
+                </TouchableOpacity>
             </View>
         </View>
     
@@ -123,5 +255,29 @@ const styles = StyleSheet.create({
     },
     profileContainer:{
         backgroundColor:'red'
+    },
+    form:{
+        backgroundColor: '#5f5ae2',
+        paddingBottom:10,
+        width:90,
+        alignItems:'center',
+        borderRadius:5,
+        marginTop:20
+    },
+    modalTitle:{
+        fontSize:10,
+        color:'white'
+    },
+    modalContainer:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    keyboardAvoidingView:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
